87756d9cd8b0927226d716cb022957023042160b
Nick Mladenov <s3539747@student.rmit.edu.au>
2017-10-06 17:36:01 +1100

    Added brief documentation & simplified the code

--- src/mazeGenerator/RecursiveBacktrackerGenerator.java
+++ src/mazeGenerator/RecursiveBacktrackerGenerator.java
@@ -4,7 +4,6 @@ import maze.Maze;
 import maze.Cell;
 import maze.Wall;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class RecursiveBacktrackerGenerator implements MazeGenerator {
@@ -16,46 +15,42 @@ public class RecursiveBacktrackerGenerator implements MazeGenerator {
         /** Local Variable Initialisation **/
         int row = rand.nextInt(maze.sizeR), col = rand.nextInt(maze.sizeC);
         /** Implementation **/
-        recursiveBacktrack(maze, null, row, col);
+        recursiveBacktrack(maze, maze.map[row][col], row, col);
 	} // end of generateMaze()
 
+    /**
+     * Recursively pave a pseudorandom generated pathway through the maze using Depth First Search (DFS) algorithm.
+     * @param maze is the maze we are constructing
+     * @param neighbour is an unvisited cell within the maze
+     * @param row holds what row in the maze the neighbour is at
+     * @param col holds what column in the maze the neighbour is at
+     */
     private void recursiveBacktrack(Maze maze, Cell neighbour, int row, int col) {
         /** Local Variable Initialisation **/
-        Cell start = null;
         int direction;
         /** Implementation **/
-        // Verify at least one wall is currently present.
-        for (Wall wall : (neighbour == null ? (start = maze.map[row][col]).wall : neighbour.wall)) {
-            if (wall != null && wall.present == true)
-                break;
-            else if (wall == (neighbour == null ? start.wall[Maze.NUM_DIR - 1] : neighbour.wall[Maze.NUM_DIR - 1]))
-                return;
-        }
-        for (;;) {
-            direction = nextDirection(maze, neighbour == null ? start : neighbour);
-            if (direction == -1)
-                break;
-            // Knock down the walls for a particular direction
-            if (neighbour != null) {
-                neighbour.wall[direction].present = false;
-                System.out.printf("Backtrack: r(%d) + dR(%d) = %d, c(%d) + dC(%d) = %d\n", row, Maze.deltaR[direction], row + Maze.deltaR[direction], col, Maze.deltaC[direction], col + Maze.deltaC[direction]);
-                recursiveBacktrack(maze, (neighbour = maze.map[row + Maze.deltaR[direction]][col + Maze.deltaC[direction]]),
-                        neighbour.r, neighbour.c);
-            } else {
-                start.wall[direction].present = false;
-                recursiveBacktrack(maze, (neighbour = maze.map[row + Maze.deltaR[direction]][col + Maze.deltaC[direction]]),
-                        neighbour.r, neighbour.c);
-            }
+        // Get a direction of an unvisited cell if any are available
+        while ((direction = nextDirection(maze, neighbour)) != -1) {
+            // Knock down the wall towards the unvisited cell
+            neighbour.wall[direction].present = false;
+            recursiveBacktrack(maze, (neighbour = maze.map[row + Maze.deltaR[direction]][col + Maze.deltaC[direction]]),
+                    neighbour.r, neighbour.c);
         }
     } // end of recursiveBacktrack()
 
+    /**
+     * Determine the next unvisited direction in the maze to progress with wall deconstruction.
+     * @param maze is the maze we are constructing
+     * @param cell is our current position in the maze
+     * @return the unvisited direction if any remain
+     */
     private int nextDirection(Maze maze, Cell cell) {
         /** Local Variable Instantiation **/
         Random rand = new Random();
         /** Local Variable Initialisation **/
         int value = rand.nextInt(Maze.NUM_DIR);
         /** Implementation **/
-        // Check all walls to determine whether there is an unvisited cell
+        // Check all walls to determine whether there is an unvisited neighbour starting from random
         for (int i = value; i < Maze.NUM_DIR; i++)
             if (cell.wall[i] != null && !isOutOfBounds(maze, cell, i) && isCellUnvisited(maze, cell, i))
                 return i;
@@ -65,6 +60,13 @@ public class RecursiveBacktrackerGenerator implements MazeGenerator {
         return -1;
     } // end of nextDirection()
 
+    /**
+     * Evaluate if the cell we would traverse to given a particular direction is currently unvisited.
+     * @param maze is the maze we are constructing
+     * @param cell is our current position in the maze
+     * @param direction is the direction we want to evaluate
+     * @return whether the cell is unvisited
+     */
     private boolean isCellUnvisited(Maze maze, Cell cell, int direction) {
         /** Implementation **/
 	    // Check whether a neighbouring cell is unvisited
@@ -74,6 +76,13 @@ public class RecursiveBacktrackerGenerator implements MazeGenerator {
         return true;
     } // end of isCellUnvisited()
 
+    /**
+     * Evaluate if the cell we would traverse to given a particular direction is outside the mazes boundaries.
+     * @param maze is the maze we're constructing
+     * @param cell is our current position in the maze
+     * @param direction is the direction we want to evaluate
+     * @return whether the direction leads out of bounds
+     */
     private boolean isOutOfBounds(Maze maze, Cell cell, int direction) {
         /** Local Variable Initialisation **/
 	    int deltaR = cell.r + Maze.deltaR[direction], deltaC = cell.c + Maze.deltaC[direction];
